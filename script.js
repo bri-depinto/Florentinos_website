e9ab8aa5500fc40786564cc61e7afc1b15ee2cee
bri.depinto <briannadepinto@gmail.com>
2025-03-31 14:54:20 -0400

    buttons

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-let vid = document.getElementById("videoBG");
-vid.playbackRate = 0.75;
+let imgHeader = document.querySelector(".img-header");
+
+let height = parseInt(getComputedStyle(imgHeader).height, 10);
+
+let nav = document.querySelector('.navbar');
+
+// Add scroll event listener
+window.addEventListener('scroll', () => {
+    if (window.scrollY > height - (height * 0.8)) {
+        nav.classList.add('navbar-scrolled');
+    } else {
+        nav.classList.remove('navbar-scrolled');
+    }
+});

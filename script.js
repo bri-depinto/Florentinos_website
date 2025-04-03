67baf8e769c4a0550a99fb8e1701989af0d0fb90
bri.depinto <briannadepinto@gmail.com>
2025-04-03 14:57:28 -0400

    finished the owners section worked on creating JSON file for menus

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            menus: [], // Store classroom data
+        };
+    },
+    created() {
+        // Fetch JSON data
+        fetch("menus.json") // Replace with actual path
+            .then(response => response.json())
+            .then(data => {
+                this.menu = data;
+            })
+            .catch(error => console.error("Error loading menus:", error));
+    }
+});
+
+vue_app.mount("#vue_app");
+
 let imgHeader = document.querySelector(".img-header");
 
 let height = parseInt(getComputedStyle(imgHeader).height, 10);

7a0ca3bf91dc47a9dc9110caa161a4c9de1d844c
bri.depinto <briannadepinto@gmail.com>
2025-04-08 13:26:08 -0400

    figured out the menus with vue

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const vue_app = Vue.createApp({
+  data() {
+      return {
+          menus: []
+      };
+  },
+  methods: {
+    openPdf(link) {
+      window.open(link, '_blank');
+    },
+  },
+  created() {
+      fetch('menus.json')
+          .then(response => response.json())
+          .then(json => {
+              this.menus = json;
+          })
+          .catch(error => console.error("Error fetching data:", error));
+  }
+});
+
+vue_app.mount("#vue_app");
+
 
 var carousel = document.querySelector('.carousel');
 var cellCount = 9;
@@ -20,37 +43,19 @@ nextButton.addEventListener( 'click', function() {
   rotateCarousel();
 });
 
-// vue_app.mount("#vue_app");
-
-// let imgHeader = document.querySelector(".img-header");
-
-// let height = parseInt(getComputedStyle(imgHeader).height, 10);
-
-// let nav = document.querySelector('.navbar');
-
-// // Add scroll event listener
-// window.addEventListener('scroll', () => {
-//     if (window.scrollY > height - (height * 0.8)) {
-//         nav.classList.add('navbar-scrolled');
-//     } else {
-//         nav.classList.remove('navbar-scrolled');
-//     }
-// });
 
 
-// const app = Vue.createApp({
-//     data() {
-//         return {
-//             menus: [], // Store classroom data
-//         };
-//     },
-//     created() {
-//         // Fetch JSON data
-//         fetch("menus.json") // Replace with actual path
-//             .then(response => response.json())
-//             .then(data => {
-//                 this.menu = data;
-//             })
-//             .catch(error => console.error("Error loading menus:", error));
-//     }
-// });
+let imgHeader = document.querySelector(".img-header");
+
+let height = parseInt(getComputedStyle(imgHeader).height, 10);
+
+let nav = document.querySelector('.navbar');
+
+// Add scroll event listener
+window.addEventListener('scroll', () => {
+    if (window.scrollY > height - (height * 0.8)) {
+        nav.classList.add('navbar-scrolled');
+    } else {
+        nav.classList.remove('navbar-scrolled');
+    }
+});

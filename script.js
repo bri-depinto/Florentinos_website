25bb7033526c2e971802771bdc6fa9f3a239bc38
bri.depinto <briannadepinto@gmail.com>
2025-04-22 14:26:16 -0400

    made the navbar disspear on click

--- script.js
+++ script.js
@@ -88,3 +88,21 @@ function scrollToSection(id) {
       behavior: "smooth"
   });
 }
+
+// Navbar collapses on click
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll(".nav-link");
+  const navbarCollapse = document.querySelector(".navbar-collapse");
+
+  navLinks.forEach(link => {
+      link.addEventListener("click", function () {
+          if (navbarCollapse.classList.contains("show")) {
+              if (typeof bootstrap !== "undefined") {
+                  new bootstrap.Collapse(navbarCollapse, {
+                      toggle: true
+                  });
+              }
+          }
+      });
+  });
+});

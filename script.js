285279c9e3a1a401087d9c14de373678e1e52ffd
bri.depinto <briannadepinto@gmail.com>
2025-04-23 11:07:49 -0400

    maybe ????

--- script.js
+++ script.js
@@ -27,44 +27,47 @@ let imgHeader = document.querySelector(".img-header");
 
 let height = parseInt(getComputedStyle(imgHeader).height, 10);
 
-// let nav = document.querySelector('.navbar');
+let nav = document.querySelector('.navbar');
 
-// window.addEventListener('scroll', () => {
-//     if (window.scrollY > height - (height * 0.8)) {
-//         nav.classList.add('navbar-scrolled');
-//     } else {
-//         nav.classList.remove('navbar-scrolled');
-//     }
-// });
+// Add scroll event listener
+window.addEventListener('scroll', () => {
+    if (window.scrollY > height - (height * 0.8)) {
+        nav.classList.add('navbar-scrolled');
+    } else {
+        nav.classList.remove('navbar-scrolled');
+    }
+});
 
-// let navHeight = parseInt(getComputedStyle(nav).height, 10);
+let navHeight = parseInt(getComputedStyle(nav).height, 10);
 
-// function scrollToSection(id) {
-//   const element = document.getElementById(id);
-//   const offset = navHeight + 75;
-//   const elementPosition = element.getBoundingClientRect().top + window.scrollY;
-//   window.scrollTo({
-//       top: elementPosition - offset,
-//       behavior: "smooth"
-//   });
-// }
+// Button on scroll function that does not cut off div
+function scrollToSection(id) {
+  const element = document.getElementById(id);
+  const offset = navHeight + 75;
+  const elementPosition = element.getBoundingClientRect().top + window.scrollY;
+  window.scrollTo({
+      top: elementPosition - offset,
+      behavior: "smooth"
+  });
+}
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   const navLinks = document.querySelectorAll(".nav-link");
-//   const navbarCollapse = document.querySelector(".navbar-collapse");
+// Navbar collapses on click
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll(".nav-link");
+  const navbarCollapse = document.querySelector(".navbar-collapse");
 
-//   navLinks.forEach(link => {
-//       link.addEventListener("click", function () {
-//           if (navbarCollapse.classList.contains("show")) {
-//               if (typeof bootstrap !== "undefined") {
-//                   new bootstrap.Collapse(navbarCollapse, {
-//                       toggle: true
-//                   });
-//               }
-//           }
-//       });
-//   });
-// });
+  navLinks.forEach(link => {
+      link.addEventListener("click", function () {
+          if (navbarCollapse.classList.contains("show")) {
+              if (typeof bootstrap !== "undefined") {
+                  new bootstrap.Collapse(navbarCollapse, {
+                      toggle: true
+                  });
+              }
+          }
+      });
+  });
+});
 
 const carousel = document.querySelector('.carousel');
 const cells = document.querySelectorAll('.carousel__cell');

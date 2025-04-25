b5bf9ea058937d306e0135bd7d81a076cbd64e08
bri.depinto <briannadepinto@gmail.com>
2025-04-25 13:25:34 -0400

    added back to top button

--- script.js
+++ script.js
@@ -103,3 +103,22 @@ document.querySelector('.next-button').addEventListener('click', () => {
 
 // Optional: Recalculate on window resize
 window.addEventListener('resize', rotateCarousel);
+
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
